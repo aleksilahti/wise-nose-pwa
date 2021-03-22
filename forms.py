1b476152dc153d47a837a9ac98d153acd5d7974d
aleksilahti <aleksi.mikael.lahti@gmail.com>
2021-03-22 05:10:05 +0200

    user creation via Contact form, password change, Admin roles etc added

--- forms.py
+++ forms.py
@@ -6,11 +6,11 @@ from app import User
 class LoginForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
-	remember = BooleanField('Remember Me')
 
 	submit = SubmitField('Login')
 
 class RegisterForm(FlaskForm):
+	name = StringField('Full name', validators=[Length(min=1, max=100)])
 	username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
 	email = StringField('Email', validators=[DataRequired(), Email()])
 	password = PasswordField('Password', validators=[DataRequired()])
@@ -27,3 +27,28 @@ class RegisterForm(FlaskForm):
 		user = User.query.filter_by(email=email.data).first()
 		if user:
 			raise ValidationError('An account with that email already exists')
+
+class ContactForm(FlaskForm):
+    name = StringField('Full name', validators=[DataRequired(), Length(min=1, max=100)])
+    username = StringField('Requested username', validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    organization = StringField('Organization', validators=[DataRequired(), Length(min=1, max=100)])
+    message = StringField('Message', validators=[DataRequired(), Length(min=1, max=255)])
+
+    submit = SubmitField('Request access')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username taken')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('An account with that email already exists')
+
+class PwForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Change password')
