b270826a5f3292d171246e2a879c92cf45760114
Jonathan <mbtjonathan@gmail.com>
2021-03-28 04:04:45 +0300

    Add Session execution layout and design logic

--- static/js/design.js
+++ static/js/design.js
@@ -61,34 +61,111 @@ $(document).ready(function(){
                }
           })
      })
-
-     $(".sample-box").not(":last-child").on("click", function(){
-          if($(this).hasClass("bg-hot")){
-               $(this).removeClass("bg-hot")
-               $(this).addClass("bg-not-hot")
+     var order = 1
+     $(".sample-box").not(":last-child").on("click", function(e){
+          e.stopPropagation()
+          $(".sample-box.active").removeClass("active")
+          $(".popup").addClass("active")
+          $(this).addClass("active")
+          sample = $(this)
+          if(sample.children().text() ==""){
+               $("#order").val(order)
           }else{
-               $(this).removeClass("bg-not-hot")
-               $(this).addClass("bg-hot")
+               $("#order").val(sample.children().text())
+          }
+     })
+     $(".sample-box.not-outlined").on("click", function(){
+          if ($(this).is("#no")){
+               sample.removeClass()
+               sample.addClass("sample-box bg-not-hot")
+          }else if ($(this).is("#interested")){
+               sample.removeClass()
+               sample.addClass("sample-box bg-interested")
+          }else if ($(this).is("#maybe")){
+               sample.removeClass()
+               sample.addClass("sample-box bg-maybe")
+          }else{
+               sample.removeClass()
+               sample.addClass("sample-box bg-hot")
+          }
+          if(sample.children().first().text() == ""){
+               order += 1
+          }
+          sample.children().first().text($("#order").val())
+          $(".popup").removeClass("active")
+          sample.removeClass("active")
+     })
+
+     $(document).on("click", function(e){
+          var $target = $(e.target);
+          if(!$target.closest('.popup').length && $('.popup').hasClass("active")) { //detect click outside the .popup div
+               $(".popup").removeClass("active")
+               $(".sample-box.active").removeClass("active")
           }
      })
 
      $(".sample-box.add").on("click", function(){
-          $(this).parent().children().not(":last-child").last().after("<div class='sample-box bg-not-hot'><p class='active'>-</p></div>")
-          $(this).parent().children().not(":last-child").last().on("click", function(){
-               if($(this).hasClass("bg-hot")){
-                    $(this).removeClass("bg-hot")
-                    $(this).addClass("bg-not-hot")
+          $(this).parent().children().not(":last-child").last().after("<div class='sample-box bg-not-hot'><p class='order'></p><p class='active'>-</p></div>")
+          $(this).parent().children().not(":last-child").last().on("click", function(e){
+               e.stopPropagation()
+               $(".sample-box.active").removeClass("active")
+               $(".popup").addClass("active")
+               $(this).addClass("active")
+               sample = $(this)
+               if($(sample.children().first()).text() ==""){
+                    $("#order").val(order)
                }else{
-                    $(this).removeClass("bg-not-hot")
-                    $(this).addClass("bg-hot")
+                    $("#order").val($(sample.children().first()).text())
                }
           })
-          $(this).parent().children().not(":last-child").last().children().on("click", function(e){
+          $(this).parent().children().not(":last-child").last().children().last().on("click", function(e){
                e.stopPropagation()
                $(this).parent().remove()
+               $(".popup").removeClass("active")
           })
      })
 
+     $("#minus").on("click", function(){
+          val = $("#order").val()
+          if(val > 1){
+               $("#order").val(val-1)
+          }
+     })
+
+     $("#add").on("click", function(){
+          val = parseInt($("#order").val())
+          $("#order").val(val + 1)
+     })
+
+     $("#previous").on("click", function(){
+          actualSelectedSample = $(".sample-box.active")
+          allSamples = $(".sample-box").not(":last-child")
+          if(allSamples.index(actualSelectedSample) != 0){
+               actualSelectedSample.removeClass("active")
+               newSample = $(allSamples[allSamples.index(actualSelectedSample)-1]).addClass("active")
+               if(newSample.text() == ""){
+                    $("#order").val(order)
+               }else{
+                    $("#order").val(newSample.text())
+               }
+               newSample.trigger("click")
+          }
+     })
+
+     $("#next").on("click", function(){
+          actualSelectedSample = $(".sample-box.active")
+          allSamples = $(".sample-box").not(":last-child")
+          if(allSamples.index(actualSelectedSample) != allSamples.length-1){
+               actualSelectedSample.removeClass("active")
+               newSample = $(allSamples[allSamples.index(actualSelectedSample)+1]).addClass("active")
+               if(newSample.text() == ""){
+                    $("#order").val(order)
+               }else{
+                    $("#order").val(newSample.text())
+               }
+               newSample.trigger("click")
+          }
+     })
 })
 
 function save(id){
