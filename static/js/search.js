580a9fe46dc35fa03765c38bfa14fe5502d418d7
svetlanapelevina <pelevinasd4@gmail.com>
2021-04-26 04:59:08 +0300

    remove old code

--- static/js/search.js
+++
@@ -1,5 +0,0 @@
-function handleSearchClick() {
-    let search_value = document.getElementById("search").value;
-    let event = new CustomEvent('do_search', {'detail': search_value});
-    window.dispatchEvent(event);
-}
